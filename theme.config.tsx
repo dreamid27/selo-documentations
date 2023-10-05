b9b8940db2b8dd76e5a9eb18b906674124774cf6
Muhammad Al Faris <muh.alfaris@gmail.com>
2023-10-05 07:01:07 +0700

    feat: data fetching

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>SELOin: Dokumentasi Bahasa Indonesia</span>,
   project: {
     link: 'https://github.com/dreamid27/selo-documentations',
   },
